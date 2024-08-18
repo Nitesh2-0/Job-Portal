e87bfe2dcda0badf0076641bc358f85658262657
Nitesh Kumar <118024456+Nitesh2-0@users.noreply.github.com>
2024-08-18 11:26:30 +0530

    Update axios.jsx

--- Frontend/src/utils/axios.jsx
+++ Frontend/src/utils/axios.jsx
@@ -1,1 +1,1 @@
-export const BASE_URL_FOR_USER = "https://job-portal-server-2ee8.onrender.com/api/v1/user";
+export const BASE_URL_FOR_USER = "https://job-portal-server-2ee8.onrender.com/api/v1/user/";
