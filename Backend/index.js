7dcfb788e059ecfcf9f38ed8cdf8e53916a912bc
Nitesh2-0 <niteshkumar61725@gmail.com>
2024-08-19 23:21:27 +0530

    Network_ERR

--- Backend/index.js
+++ Backend/index.js
@@ -17,17 +17,19 @@ const PORT = process.env.PORT || 3000;
 // Databse-Connection 
 connectDB();
 
+// it require to access outside
+const corsOption = {
+  origin: "http://localhost:5173",
+  credentials: true
+};
+app.use(cors(corsOption));
+
 // middleware 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(morgan('tiny'));
-const corsOption = {
-  origin: "http://localhost:5173/",
-  credentials: true
-};
-app.use(cors(corsOption));
 
 
 // api's 
@@ -35,5 +37,6 @@ app.use('/api/v1/user', userRoute);
 app.use('/api/v1/company', companyRoute);
 app.use('/api/v1/job', jobRoute);
 app.use('/api/v1/application', applicationRoute);
+
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT} 👍👍👍`));
