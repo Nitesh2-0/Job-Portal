e03c2caba2532aa393e4ce784f6fc8399c722e57
Nitesh2-0 <niteshkumar61725@gmail.com>
2024-08-24 18:02:18 +0530

    populated-getSingleJob

--- Backend/controllers/job.controller.js
+++ Backend/controllers/job.controller.js
@@ -67,7 +67,7 @@ export const getAllJob = async (req, res) => {
 export const getJobById = async (req, res) => {
   try {
     const jobId = req.params.id;
-    const job = await Job.findById(jobId)
+    const job = await Job.findById(jobId).populate('applications').populate('company'); 
     if (!job) {
       return res.status(404).json({
         message: "Job not found.",
